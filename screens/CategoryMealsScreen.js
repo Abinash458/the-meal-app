7ea97c1b5a2cc48ca29b8863cc64da98440f6b87
Abinash Mohapatra <abinash@expensebit.com>
2020-09-27 10:11:24 +0530

    configure stack navigation

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,31 +1,19 @@
 import React from 'react';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 import { CATEGORIES, MEALS } from '../data/dummy-data';
+import MealList from "../components/MealList";
 
 const CategoryMealsScreen = props => {
-
-    const renderMealItem = itemData => {
-        return (
-            <View>
-                <Text>
-                    {itemData.item.title}
-                </Text>
-            </View>
-        )
-    }
 
     const catId = props.navigation.getParam('categoryId');
 
     const displayedMeals = MEALS.filter(meal => meal.categoryIds.indexOf(catId) >= 0)
 
     return (
-        <View style={styles.screen}>
-            <FlatList
-                data={displayedMeals}
-                renderItem={renderMealItem}
-            />
-        </View>
+        <MealList
+            listData={displayedMeals}
+            navigation={props.navigation}
+        />
     );
 }
 
@@ -38,13 +26,5 @@ CategoryMealsScreen.navigationOptions = (navigationData) => {
         headerTitle: selectedCategories.title
     }
 }
-
-const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: 'center',
-    }
-});
 
 export default CategoryMealsScreen;
